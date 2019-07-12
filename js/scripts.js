dbc34975cc7248ddc7dc09cb620b28f67c448482
Maxwel5 <maxwellwafulasim@gmail.com>
2019-07-12 17:21:24 +0300

    providing variables names

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+<script Language="JavaScript">
+    <!-- start script
+    var males=["Kwasi" "Kwandwo" "Kwabena" "Kwaku" "Yaw" "Kofi" "Kwame"]
+    var females=["Akosua" "Adwoa" "Abenaa" "Akua" "Yaa" "Afua" "Ama"]
+    //end script-->
+</script>
